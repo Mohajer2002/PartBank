3107bb1cf15ec0ac09098f4c84701e41f58ea8df
mohajer <senator.x.x85@gmail.com>
2024-08-12 21:59:55 +0330

    feat : create main dashboard content layout

---
+++ src/components/views/dashboard/DashboardContent.vue
@@ -0,0 +1,16 @@
+<script setup></script>
+<template>
+  <section class="main-dashboard__dashboard-content dashboard-content">
+    <!-- no content section -->
+    <!-- dashboard content section -->
+  </section>
+</template>
+
+<style scoped lang="scss">
+.dashboard-content {
+  @include global.customFlex(column, center, stretch);
+  width: 100%;
+  color: var(--text-title);
+  position: relative;
+}
+</style>
