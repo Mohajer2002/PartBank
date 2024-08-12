97131c1bb04ee2dbf2f65a09370829becc5693c0
mohajer <senator.x.x85@gmail.com>
2024-08-12 21:59:20 +0330

    feat : create dashboard layout

---
+++ src/layouts/DashboardLayout.vue
@@ -0,0 +1,17 @@
+<script setup>
+// defineSlots(['header', 'main'])
+</script>
+<template>
+  <slot name="header"></slot>
+  <div class="dashboard-container">
+    <slot name="main"></slot>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.dashboard-container {
+  padding: 1rem 5rem 1rem 5rem;
+  min-height: calc(100vh - global.$header-height);
+  background-color: var(--basic-background);
+}
+</style>
