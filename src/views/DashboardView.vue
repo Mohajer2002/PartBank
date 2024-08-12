e820bee70063b8d6ed364d36e0c6ff45b8c40a38
mohajer <senator.x.x85@gmail.com>
2024-08-12 21:59:02 +0330

    feat : add base dashboard layout

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,6 +1,26 @@
-<script setup></script>
+<script setup>
+import TheHeader from '@/components/common/TheHeader.vue'
+import SideMenu from '@/components/views/dashboard/SideMenu.vue'
+import DashboardLayout from '@/layouts/DashboardLayout.vue'
+</script>
 <template>
-  <main></main>
+  <dashboard-layout>
+    <template v-slot:header>
+      <TheHeader />
+    </template>
+    <template v-slot:main>
+      <main class="main-dashboard">
+        <SideMenu />
+        <!-- dashboard-content -->
+      </main>
+    </template>
+  </dashboard-layout>
 </template>
 
-<style scoped lang="sass"></style>
+<style scoped lang="scss">
+.main-dashboard {
+  display: flex;
+  gap: 1rem;
+  height: 100%;
+}
+</style>
