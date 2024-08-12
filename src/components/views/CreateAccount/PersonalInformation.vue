a42a9923cc88709c648b55bb1213fa698a8d38f1
atieh.zargar <atieh.zargar@partsoftware.com>
2024-08-12 14:59:36 -0700

    feat : create object of inputs

--- src/components/views/CreateAccount/PersonalInformation.vue
+++ src/components/views/CreateAccount/PersonalInformation.vue
@@ -1,10 +1,54 @@
+<script setup>
+import { ref } from 'vue'
 
+import CustomInput from '@/components/common/CustomInput.vue'
 
-<script>
-export default {}
+const personalInformationInputs = ref([
+  {
+    name: 'firstname',
+    label: 'نام',
+    placeholder: 'نام فارسی',
+    class: 'md-width',
+    type: 'input',
+    value: null
+  },
+  {
+    name: 'lastname',
+    label: 'نام خانوادگی',
+    placeholder: 'نام خانوادگی به صورت کامل',
+    class: 'md-width',
+    type: 'input',
+
+    value: null
+  },
+
+  {
+    name: 'code',
+    label: 'کدپستی',
+    placeholder: 'کدپستی',
+    class: 'md-width',
+    type: 'input',
+    value: null
+  },
+  
+  {
+    name: 'address',
+    label: 'محل سکونت',
+    placeholder: 'محل سکونت',
+    class: 'md-width',
+    type: 'textarea',
+    value: null
+  },
+])
 </script>
 <template>
-  <div>PersonalInformation.vue</div>
+  <div class="form-group">
+ 
+  </div>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+.form-group {
+  @include global.customFlex(row, space-between, center, 2rem);
+}
+</style>
