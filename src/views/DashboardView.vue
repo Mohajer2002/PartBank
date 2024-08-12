78544291982242cfe01f1b48cbf009311f4ec121
mohajer <senator.x.x85@gmail.com>
2024-08-12 09:36:27 +0330

    feat : create dashboard view

---
+++ src/views/DashboardView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import sideMenu from '@/components/views/dashboard/SideMenu.vue'
+</script>
+<template>
+  <main>
+    <sideMenu />
+  </main>
+</template>
+
+<style scoped lang="sass"></style>
