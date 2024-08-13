c6a73cd99d1222abfb364810917cc00b67fbb0b2
atieh.zargar <atieh.zargar@partsoftware.com>
2024-08-13 05:18:29 -0700

    feat : add scss style

--- src/components/views/CreateAccount/ConfirmInformation.vue
+++ src/components/views/CreateAccount/ConfirmInformation.vue
@@ -30,6 +30,51 @@
         </section>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.information-form {
+  display: flex;
+  justify-content: space-between;
+  gap: 2rem;
+  flex-wrap: wrap;
+
+  &__form-content {
+    display: flex;
+    flex-direction: column;
+    row-gap: 0.25rem;
+    width: 24rem;
+    &--address {
+      flex-basis: 100%;
+    }
+  }
+
+  &__title {
+    color: var(--text-gray);
+    font-size: 0.875rem;
+    font-weight: 600;
+    padding-right: 0.5rem;
+  }
+  &__input {
+    background-color: #f9fafb;
+    border-radius: 0.375rem;
+    color: var(--black-500);
+    border: 1px solid #ececee;
+    height: 3rem;
+    padding: 0rem 0.5rem;
+    font-size: 0.875rem;
+    font-weight: 600;
+
+    &--address {
+      resize: none;
+      height: 7.5rem;
+      padding-top: 1rem;
+    }
+  }
+
+  &__value {
+    color: var(--black-500);
+    padding-right: 0.5rem;
+    font-weight: 600;
+  }
+}
 
 </style>
