80ad329a17c6523a9d5f6a44c33a5bfcc836269b
mohajer <senator.x.x85@gmail.com>
2024-08-13 22:37:19 +0330

    feat : create login config

---
+++ src/services/apiConfings.js
@@ -0,0 +1,12 @@
+export const loginConfig = {
+  method: 'post',
+  url: 'auth/login',
+  type: 'login',
+  headers: {
+    'gateway-token': null
+  }
+  //   data: JSON.stringify({
+  //     phoneNumber: telInputValue,
+  //     password: passwordInputValue
+  //   })
+}
