3c32ad98a2074e7b746882b91c42dda42bb54eb2
atieh.zargar <atieh.zargar@partsoftware.com>
2024-08-13 06:34:07 -0700

    feat : add html tags

--- src/components/views/CreateAccount/ConfirmInformation.vue
+++ src/components/views/CreateAccount/ConfirmInformation.vue
@@ -1,33 +1,27 @@
-<script>
-
-</script>
+<script></script>
 <template>
-        <section class="confirm-information">
-          <div class="information-form">
-            <div class="information-form__form-content">
-              <span class="information-form__title"> نام: </span>
-              <span class="information-form__value" id="firstname">بهنام</span>
-            </div>
-            <div class="information-form__form-content">
-              <span class="information-form__title"> نام خانوادگی: </span>
-              <span class="information-form__value" id="lastname"> وهانی </span>
-            </div>
-            <div class="information-form__form-content">
-              <span class="information-form__title"> کدپستی: </span>
-              <span class="information-form__value" id="code">
-                ۹۱۷۵۶۸۷۴۲۳
-              </span>
-            </div>
-            <div
-              class="information-form__form-content information-form__form-content--address"
-            >
-              <span class="information-form__title"> محل سکونت: </span>
-              <span class="information-form__value" id="address">
-                بولوار ملک آباد ، خیام جنوبی ۱۳ ، گلایل ۱۰ ، پلاک۱۲۳ ، واحد۱
-              </span>
-            </div>
-          </div>
-        </section>
+  <section class="confirm-information">
+    <div class="information-form">
+      <div class="information-form__form-content">
+        <span class="information-form__title"> نام: </span>
+        <span class="information-form__value" id="firstname">بهنام</span>
+      </div>
+      <div class="information-form__form-content">
+        <span class="information-form__title"> نام خانوادگی: </span>
+        <span class="information-form__value" id="lastname"> وهانی </span>
+      </div>
+      <div class="information-form__form-content">
+        <span class="information-form__title"> کدپستی: </span>
+        <span class="information-form__value" id="code"> ۹۱۷۵۶۸۷۴۲۳ </span>
+      </div>
+      <div class="information-form__form-content information-form__form-content--address">
+        <span class="information-form__title"> محل سکونت: </span>
+        <span class="information-form__value" id="address">
+          بولوار ملک آباد ، خیام جنوبی ۱۳ ، گلایل ۱۰ ، پلاک۱۲۳ ، واحد۱
+        </span>
+      </div>
+    </div>
+  </section>
 </template>
 
 <style lang="scss" scoped>
@@ -76,5 +70,4 @@
     font-weight: 600;
   }
 }
-
 </style>
--- src/components/views/CreateAccount/UploadCard.vue
+++ src/components/views/CreateAccount/UploadCard.vue
@@ -2,9 +2,114 @@
 
 </script>
 <template>
-  <div>
-    upload card
-  </div>
+  <section class="upload-files-section">
+          <div class="file-section">
+            <label
+              for="input-file1"
+              class="file-section__upload-section file-section__upload-section--front upload-box"
+            >
+              <img alt="front image" class="upload-box__preview" />
+              <img
+                src="../../../public/assets/icons/upload.svg"
+                alt="upload icon"
+                class="upload-box__icon"
+              />
+              <p class="upload-box__text-helper">
+                تصویر را بکشید و اینجا رها کنید یا
+                <span class="upload-box__text-helper--important">کلیک کنید</span
+                >.
+              </p>
+              <input
+                type="file"
+                accept="image/*"
+                id="input-file1"
+                class="upload-box__input-file"
+              />
+            </label>
+            <div class="file-section__detail">
+              <p class="file-section__description">تصویر روی کارت ملی</p>
+              <button
+                type="button"
+                class="file-section__option file-section__option--active"
+              >
+                <img
+                  src="../../../public/assets/icons/more-option.svg"
+                  alt="moreIcon"
+                />
+                <ul class="tools-menu tools-menu--transition">
+                  <li class="tools-menu__item">
+                    <img
+                      src="../../../public/assets/icons/edit.svg"
+                      alt="icon"
+                    />
+                    <p>ویرایش</p>
+                  </li>
+                  <div class="tools-menu__divider"></div>
+                  <li class="tools-menu__item tools-menu__item--warring">
+                    <img
+                      src="../../../public/assets/icons/trash.svg"
+                      alt="icon"
+                    />
+                    <p>حذف</p>
+                  </li>
+                </ul>
+              </button>
+            </div>
+          </div>
+          <div class="file-section">
+            <label
+              for="input-file2"
+              class="file-section__upload-section file-section__upload-section--back upload-box"
+            >
+              <img alt="back image" class="upload-box__preview" />
+              <img
+                src="../../../public/assets/icons/upload.svg"
+                alt="upload icon"
+                class="upload-box__icon"
+              />
+              <p class="upload-box__text-helper">
+                تصویر را بکشید و اینجا رها کنید یا
+                <span class="upload-box__text-helper--important">کلیک کنید</span
+                >.
+              </p>
+              <input
+                type="file"
+                accept="image/*"
+                id="input-file2"
+                class="upload-box__input-file"
+              />
+            </label>
+            <div class="file-section__detail">
+              <p class="file-section__description">تصویر پشت کارت ملی</p>
+              <button
+                type="button"
+                class="file-section__option file-section__option--active"
+              >
+                <img
+                  src="../../../public/assets/icons/more-option.svg"
+                  alt="moreIcon"
+                />
+                <ul class="tools-menu tools-menu--transition">
+                  <li class="tools-menu__item">
+                    <img
+                      src="../../../public/assets/icons/edit.svg"
+                      alt="icon"
+                    />
+                    <p>ویرایش</p>
+                  </li>
+                  <div class="tools-menu__divider"></div>
+                  <li class="tools-menu__item tools-menu__item--warring">
+                    <img
+                      src="../../../public/assets/icons/trash.svg"
+                      alt="icon"
+                    />
+                    <p>حذف</p>
+                  </li>
+                </ul>
+              </button>
+            </div>
+          </div>
+        </section>
 </template>
 
 <style>
