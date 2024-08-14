93a99ad946c5a9f40ad0e804d0f8449f712f5e9d
mohajer <senator.x.x85@gmail.com>
2024-08-14 12:05:38 +0330

    feat : improve fetch api

--- src/services/api.js
+++ src/services/api.js
@@ -1,22 +1,24 @@
 import { ref } from 'vue'
 import { axiosInstance } from './apiSetting'
 
-export const fetchData = async (config) => {
-  const data = ref(null)
+export function useFetch(config) {
+  const responseData = ref(null)
   const error = ref(null)
-  const loading = ref(false)
-  loading.value = true
-  error.value = null
-  try {
-    const response = await axiosInstance(config)
-    data.value = response.data
-  } catch (error) {
-    error.value = error.message
-    console.log('show me error', error)
-  } finally {
-    loading.value = false
-  }
-  //   console.log(data.value)
+  const isLoading = ref(false)
 
-  return { data, error, loading }
+  async function fetchData(config) {
+    isLoading.value = true
+    error.value = null
+    try {
+      const response = await axiosInstance(config)
+      responseData.value = response.data
+    } catch (error) {
+      error.value = error.message
+    } finally {
+      isLoading.value = false
+    }
+  }
+  fetchData(config)
+
+  return { responseData, error, isLoading }
 }
