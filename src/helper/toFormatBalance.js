6f772da364e2231f5e756810e0dbd22131c4449f
mohajer <senator.x.x85@gmail.com>
2024-08-15 13:07:27 +0330

    feat : delete unusable code

--- src/helper/toFormatBalance.js
+++ src/helper/toFormatBalance.js
@@ -2,7 +2,5 @@ import formatNumberWithSeparator from './formatNumberWithSeparator'
 import toPersianDigit from './toPersianDigit'
 
 export default function toFormatBalance(balance) {
-  console.log(typeof balance)
-
   return toPersianDigit(formatNumberWithSeparator(balance.toString()))
 }
