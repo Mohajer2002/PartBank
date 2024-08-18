a6d021a5a09721bf5cb213bfe7c1d6342aa092ed
mohajer <senator.x.x85@gmail.com>
2024-08-18 11:55:33 +0330

    fix : prevent undefinde value

--- src/helper/toPersianDigit.js
+++ src/helper/toPersianDigit.js
@@ -1,3 +1,3 @@
 export default function toPersianDigit(englishDigit) {
-  return englishDigit.toString().replace(/\d/g, (digit) => '۰۱۲۳۴۵۶۷۸۹'[digit])
+  return englishDigit?.toString().replace(/\d/g, (digit) => '۰۱۲۳۴۵۶۷۸۹'[digit])
 }
